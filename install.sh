96950acc17d7e6a4079df79b31b9b390373ff3bc
Uplink036 <oliversjodin02@gmail.com>
2025-03-10 13:40:55 +0000

    Update install script

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
 sudo apt update 
 sudo apt upgrade -y 
+sudo apt install bc
 make python27
 make pip27
 make req
+
+sudo apt install python3-pip -y
+pip install -r noteboke_req.txt
+python3 -m pip install ipykernel -U --user --force-reinstall
